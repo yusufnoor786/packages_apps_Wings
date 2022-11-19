7f62022d31872cbe5339297a465bf6f27eaa141c
ralf979 <ralf979@gmail.com>
2022-11-19 10:45:09 +0100

    Pextension: rename to pextension

--- src/org/pixelextended/pextension/fragments/PulseSettings.java
+++ src/org/pixelextended/pextension/fragments/PulseSettings.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.pixelextended.snowhouse.fragments;
+package org.pixelextended.pextension.fragments;
 
 import android.content.Context;
 import android.content.ContentResolver;
